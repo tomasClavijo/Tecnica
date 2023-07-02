379a1a1273079485894e6aaacb4bbebfdc13e3d2
Tomás <89207931+tomasClavijo@users.noreply.github.com>
2023-07-02 00:39:37 -0300

    Update solution.js

--- solution.js
+++ solution.js
@@ -41,3 +41,75 @@ function getBriefMessages(objectArray){
     
     return shortMessages.concat(getBriefMessages(nextMessage));
 }
+
+// Ejercicio 3
+
+function fillSummaries(objectArray){
+    let result = [];
+
+    function processObject(i){
+        if (i === objectArray.length){
+            return;
+        }
+
+        let message = objectArray[i].message;
+        let messageLength = message.length;
+        let newObject = null;
+
+        if (messageLength < 60){
+            newObject = {
+                message: message,
+                id : objectArray[i].id,
+                trimmed: message
+            };
+        } else {
+            let trimmed = message.slice(0, 60);
+            newObject = {
+                message: message,
+                id: objectArray[i].id,
+                trimmed: trimmed
+            };
+        }
+
+        result.push(newObject);
+        processObject(i + 1);
+    }
+
+    processObject(0);
+    return result;
+}
+
+// Ejercicio 4
+
+function addSummaries(stringArray){
+    let result = [];
+
+    function processString(i){
+        if (i === stringArray.length){
+            return;
+        }
+
+        let text = stringArray[i];
+        let textLength = text.length;
+        let newObject = null;
+
+        if(textLength < 60){
+            newObject = {
+                message: text,
+                trimmed: text
+            };
+        } else {
+            let trimmed = text.slice(0, 60);
+            newObject = {
+                message: text,
+                trimmed: trimmed
+            };
+        }
+
+        result.push(newObject);
+        processString(i + 1);
+    }
+
+    processString(0);
+    return result;
+}
